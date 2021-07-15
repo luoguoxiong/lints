8926696ca678494ec8394733a36e1dd8dab3624f
罗国雄 <1025558554@qq.com>
2021-07-15 15:15:22 +0800

    feat: other

--- packages/stylelint-config-self/index.js
+++ packages/stylelint-config-self/index.js
@@ -1,8 +1,6 @@
 module.exports = {
     "extends": "stylelint-config-standard",
     "plugins": ["stylelint-prettier"],
-    "rules": {
-        "prettier/prettier": true
-      }
-  }
+    "rules": {}
+}
   
